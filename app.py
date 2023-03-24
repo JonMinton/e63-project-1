dd2629e76167bf2bc010d6020c502859462c5690
Jon Minton <jon.will.minton@gmail.com>
2023-03-24 10:30:19 +0000

    build dummy files in dir structure

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+#  ################################################################
+# Put controllers here : examples below
+# from controllers.authors_controller import authors_blueprint
+# from controllers.books_controller import books_blueprint
+# from controllers.authors_controller import authors_blueprint
+
+app = Flask(__name__)
+
+# Register controllers here. Examples below
+# app.register_blueprint(authors_blueprint)
+# app.register_blueprint(books_blueprint)
+# app.register_blueprint(authors_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
